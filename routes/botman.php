a811f7dfe160d0e7fb4c845884efbb91d339c946
David Llop <d.lloople@gmail.com>
2018-03-01 23:15:05 +0100

    Remove useless route

--- routes/botman.php
+++ routes/botman.php
@@ -4,19 +4,14 @@ $botman = resolve('botman');
 
 $botman->middleware->received(new \App\Http\Middleware\Botman\LoadUserMiddleware());
 
-$botman->hears('user', function (\BotMan\BotMan\BotMan $bot) {
-    $bot->reply("Hello ".auth()->user()->username);
-});
-
 $botman->hears('/start|hola|afegir estacio', 'App\Http\Controllers\GirocletaController@registerConversation');
 
-$botman->hears('/station|^estaci[ó|o]|quina .* estaci[ó|o]', 'App\Http\Controllers\GirocletaController@checkStation');
+$botman->hears('/station|^estaci[ó|o]$|quina .* estaci[ó|o]', 'App\Http\Controllers\GirocletaController@checkStation');
 
 $botman->hears('afegir recordatori|/reminder', 'App\Http\Controllers\GirocletaController@reminderConversation');
 
 $botman->hears('els meus recordatoris|/reminders', 'App\Http\Controllers\GirocletaController@seeReminders');
 
-
-$botman->hears('/remove|/forget|/delete|(?:borrar?|treu(?:re)?|oblidar?) usuari', 'App\Http\Controllers\GirocletaController@deleteUser');
+$botman->hears('/remove|/forget|/delete|(?:borrar?|oblidar?) usuari', 'App\Http\Controllers\GirocletaController@deleteUser');
 
 $botman->receivesLocation('App\Http\Controllers\GirocletaController@nearStations');
