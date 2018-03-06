534739d45b7d8ff8a98a8a33a2c0e5cddb0c8f74
David Llop <d.lloople@gmail.com>
2018-03-06 22:35:10 +0100

    Fix some bugs with botman routes

--- routes/botman.php
+++ routes/botman.php
@@ -8,7 +8,7 @@ $botman->hears('/help|^ajuda$|(?:no se )?(?:que fer|com (?:funciona|va))', 'App\
 
 $botman->hears('/station|^estaci[ó|o]$|^hola$|👋', 'App\Http\Controllers\GirocletaController@greetings');
 $botman->hears('/start|(?:afegir|definir|canviar?) estaci[ó|o]', 'App\Http\Controllers\GirocletaController@registerConversation');
-$botman->hears('(?:(?:vull)? anar de )?(.*) a (.*)', 'App\Http\Controllers\GirocletaController@tripInformation');
+$botman->hears('(?:(?:vull)? anar de |de )?(.*) a (.*)', 'App\Http\Controllers\GirocletaController@tripInformation');
 $botman->receivesLocation('App\Http\Controllers\GirocletaController@nearStations');
 
 $botman->hears('^/reminders$|els meus recordatoris|recordatoris', 'App\Http\Controllers\RemindersController@index');
@@ -16,9 +16,9 @@ $botman->hears('^/reminder$|(?:afegir|definir|crear) recordatori', 'App\Http\Con
 $botman->hears('^/reminderdelete$|(?:esborrar?|treu[re]?|oblidar?) recordatori', 'App\Http\Controllers\RemindersController@destroy');
 
 $botman->hears('/aliases|els meus alias|veure alias', 'App\Http\Controllers\AliasesController@index');
-$botman->hears('/alias|(?:afegir|definir|crear?) alias', 'App\Http\Controllers\AliasesController@create');
+$botman->hears('/alias$|(?:afegir|definir|crear?) alias', 'App\Http\Controllers\AliasesController@create');
 $botman->hears('^/aliasdelete|(?:esborrar?|treu[re]?|oblidar?) alias', 'App\Http\Controllers\AliasesController@destroy');
 
 $botman->hears('/remove|/forget|/delete|(?:borrar?|oblidar?) usuari', 'App\Http\Controllers\UsersController@destroy');
 
-$botman->hears('(.*)', 'App\Http\Controllers\GirocletaController@fallback');
+$botman->fallback('App\Http\Controllers\FallbackController@index');
