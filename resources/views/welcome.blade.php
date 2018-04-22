8f0df1a10170d73d0aca65e7ecda92255e35ec43
David Llop <d.lloople@gmail.com>
2018-04-22 16:20:38 +0200

    Add more information to landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,37 +19,40 @@
     <h1>BOT DE TELEGRAM PER LA GIROCLETA</h1>
     <div class="flex flex-wrap mt-8">
         <div class="md:w-1/2">
-            <h2 class="mb-8">Escull una estació</h2>
-            <p class="md:text-right">
-                Pots triar una estació com a <strong>preferida</strong>. D'aquesta manera cada cop que saludis
-                el
-                bot et donarà la informació en temps real.
-            </p>
+            <div class="panel-info">
+                <h2 class="panel-title">Escull una estació</h2>
+                <p>
+                    Pots triar una estació com a <strong>preferida</strong>. D'aquesta manera cada cop que saludis
+                    el bot et donarà la informació en temps real.
+                </p>
+                <img src="{{ asset('img/screenshots/start.png') }}" alt="Escull una parada">
+            </div>
+            <div class="panel-info">
+                <h2 class="panel-title">Demana per una estació</h2>
+                <p>Pots demanar informació per una estació en concret en qualsevol moment fent servir el nom o una
+                    localització propera com un carrer o un lloc emblemàtic</p>
+                <p>Junt amb la informació de les bicis disponibles i els parkings lliures, també es mostra la distància
+                    que hi ha des de l'estació escollida fins al punt que hem indicat, si l'estació ha estat trobada
+                    fent servir una localització propera.</p>
+                <img src="{{ asset('img/screenshots/naming.png') }}" alt="Busca estacions per nom">
+            
+            </div>
         </div>
         <div class="md:w-1/2">
-            <img src="{{ asset('img/screenshots/start.png') }}" alt="Escull una parada">
-        </div>
-    </div>
-    
-    <div class="flex flex-wrap mt-4">
-        <div class="md:w-1/2">
-            <img src="{{ asset('img/screenshots/reminders.png') }}" alt="Afegeix recordatoris">
-        </div>
-        <div class="md:w-1/2">
-            <h2 class="mb-8">Afegeix recordatoris</h2>
-            <div class="md:text-left">
+            <div class="panel-info">
+                <h2 class="panel-title">Afegeix recordatoris</h2>
                 <p>
                     Amb el Bot de la Girocleta pots crear recordatoris que t'avisaràn automàticament de l'estat
-                    de
-                    l'estació escollida.
+                    de l'estació escollida.
                 </p>
                 <p>Pots configurar:</p>
-                <ul>
-                    <li>Quin tipus d'avís vols (Bicis lliures o aparcaments lliures)</li>
-                    <li>De quina estació vols veure informació</li>
-                    <li>Els dies de la setmana</li>
-                    <li>L'hora del dia</li>
+                <ul class="list-reset text-left ml-8">
+                    <li>☑️ Quin tipus d'avís vols (Bicis lliures o aparcaments lliures)</li>
+                    <li>☑️ De quina estació vols veure informació</li>
+                    <li>☑️ Els dies de la setmana</li>
+                    <li>☑️ L'hora del dia</li>
                 </ul>
+                <img src="{{ asset('img/screenshots/reminders.png') }}" alt="Afegeix recordatoris">
             </div>
         </div>
     </div>
