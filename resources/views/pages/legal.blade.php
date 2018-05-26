e6314214c47532b486ca9220014529885b140b6d
David Llop <d.lloople@gmail.com>
2018-05-26 14:00:17 +0200

    Add message "we don't store user's messages"

--- resources/views/pages/legal.blade.php
+++ resources/views/pages/legal.blade.php
@@ -13,6 +13,8 @@
             </ul>
             <p>Aquestes dades no es comparteixen amb tercers i són totalment necessàries pel funcionament del bot i
                 per oferir una experiència més personalitzada a l'usuari.</p>
+            <p>Cap dels missatges que s'envia queda enregistrat de cap manera al servidor on s'allotja el bot, només al
+                historial de conversació del dispositiu de l'usuari que l'ha enviat.</p>
             <h1 class="mb-8 text-center">Esborrar les meves dades</h1>
             <p>El bot ofereix l'opció d'esborrar totes les dades relacionades amb el nostre usuari, només cal dir-li
                 <code>Esborrar usuari</code> i ens demanarà confirmació.</p>
