fe22c323446a96669ab36863c672b7526e2bb6d6
David Llop <d.lloople@gmail.com>
2018-06-01 22:30:06 +0200

    Improve information code, change font color to black

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,16 +3,14 @@
 @section('content')
     <div class="flex flex-wrap mt-8">
         <div class="md:w-1/2">
-        @foreach ($left as $leftInformation)
-            @include('information.' . str_replace('.blade.php', '', basename($leftInformation)))
-        @endforeach
-        
+            @foreach ($left as $leftInformation)
+                @include($leftInformation)
+            @endforeach
         </div>
         <div class="md:w-1/2">
-        @foreach($right as $rightInformation)
-                @include('information.' . str_replace('.blade.php', '', basename($rightInformation)))
-    
-        @endforeach
+            @foreach($right as $rightInformation)
+                @include($rightInformation)
+            @endforeach
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 Route::get('/', function () {
 
-    [$left, $right] = collect(glob(resource_path('views/information').'/*.php'))->split(2);
+    [$left, $right] = collect(glob(resource_path('views/information') . '/*.php'))
+        ->map(function ($file) {
+            return 'information.' . str_replace('.blade.php', '', basename($file));
+        })->partition(function ($file, $index) {
+            return $index % 2 == 0;
+        });
 
     return view('pages.home', compact('left', 'right'));
 
